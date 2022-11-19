13529366732684c495f9ade49cf8daa4dd321924
khuonglaptri3 <86360215+khuonglaptri3@users.noreply.github.com>
2022-11-20 01:05:58 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Cô Giáo Thu Yến ';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-11';
+const titleCard = 'Tăng Người Mẹ Thứ 2';
+const contentCard = ' Nhân Ngày Nhà giáo Việt Nam 20/11, với tất cả những lời chúc chân thành mà em muốn gửi đến các thầy cô, kính chúc các thầy cô luôn vui vẻ tràn đầy niềm tin vào cuộc sống, luôn gặp may mắn mỗi ngày. và thành công trên con đường giảng dạy của mình, kèm cặp học sinh rất tốt cho cuộc sống sau này.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
